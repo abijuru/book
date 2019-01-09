4e41e4ecf645668358e8e633d1f0b391d159e7fc
abijuru <abijuruelisa@gmail.com>
2019-01-09 04:43:54 -0500

    add the code in javascript

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-
 $(document).ready(function() {
+
     $("#add-address").click(function() {
       $("#new-addresses").append('<div class="new-address">' +
                                    '<div class="form-group">' +
@@ -16,23 +16,43 @@ $(document).ready(function() {
                                    '</div>' +
                                  '</div>');
     });
-
-    $(".new-address").each(function() {
+  
+    $("form#new-contact").submit(function(event) {
+      event.preventDefault();
+  
+      var inputtedFirstName = $("input#new-first-name").val();
+      var inputtedLastName = $("input#new-last-name").val();
+      var newContact = new Contact(inputtedFirstName, inputtedLastName);
+  
+      $(".new-address").each(function() {
         var inputtedStreet = $(this).find("input.new-street").val();
         var inputtedCity = $(this).find("input.new-city").val();
         var inputtedCounty = $(this).find("input.new-county").val();
-        var newAddress = new Address(inputtedStreet, inputtedCity, inputtedCounty);
-        newContact.addresses.push(newAddress);
+        var newAddress = new Address(inputtedStreet, inputtedCity, inputtedCounty)
+        newContact.addresses.push(newAddress)
       });
-
-
-      $("ul#addresses").text("");
-      newContact.addresses.forEach(function(address) {
-        $("ul#addresses").append("<li>" + address.street + ", " + address.city + " " + address.County + "</li>");
+  
+      $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+  
+      $(".contact").last().click(function() {
+        $("#show-contact").show();
+        $("#show-contact h2").text(newContact.fullName());
+        $(".first-name").text(newContact.firstName);
+        $(".last-name").text(newContact.lastName);
+        $("ul#addresses").text("");
+        newContact.addresses.forEach(function(address) {
+          $("ul#addresses").append("<li>" + address.street + ", " + address.city + " " + address.county + "</li>");
+        });
       });
-
-
-
-
+  
+      $("input#new-first-name").val("");
+      $("input#new-last-name").val("");
+      $("input.new-street").val("");
+      $("input.new-city").val("");
+      $("input.new-county").val("");
+  
+    });
+  });
+  
 
 
